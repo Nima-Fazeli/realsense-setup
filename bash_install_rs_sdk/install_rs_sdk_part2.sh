354f765eae4e1fb0f0138112584aff506b30e56c
Nima Fazeli <nimfazeli@gmail.com>
2018-07-25 09:44:54 -0400

    edited

--- bash_install_rs_sdk/install_rs_sdk_part2.sh
+++ bash_install_rs_sdk/install_rs_sdk_part2.sh
@@ -4,10 +4,6 @@
 ## RealSense 2.0 SDK Installer
 ##############################
 
-# Add Intel server to the list of repositories:
-echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main' | sudo tee /etc/apt/sources.list.d/realsense-public.list
-apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE
-
 sudo apt-get update
 
 sudo apt-get install librealsense2-dkms
